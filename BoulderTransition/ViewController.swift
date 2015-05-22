0bf8ba020a959129c1378376dd5895306c5da598
CHENCHIAN <KickerChen@Kickers-MacBook-Pro.local>
2015-05-22 18:54:36 +0800

    Initial Commit

---
+++ BoulderTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoulderTransition
+//
+//  Created by CHENCHIAN on 5/22/15.
+//  Copyright (c) 2015 KICKERCHEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
